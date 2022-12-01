112a4206f88c46ab7d66d4c89576b342cb56d171
ChristianNomena <christiangrzk@gmail.com>
2022-12-01 21:43:00 +0300

    add jump zone

--- scripts/ball.gd
+++ scripts/ball.gd
@@ -10,10 +10,12 @@ extends RigidBody3D
 @onready var level_info = get_node("/root/LevelInfo")
 
 var coins
+var able_to_jump
 
 
 func _ready() -> void:
 	coins = 0
+	able_to_jump = false
 
 
 func _physics_process(delta) -> void:
@@ -59,7 +61,7 @@ func _physics_process(delta) -> void:
 	camera.rotation.x = 0
 	
 	var is_on_floor = $CheckFloor.is_colliding()
-	if Input.is_action_just_pressed("player_jump") and is_on_floor:
+	if Input.is_action_just_pressed("player_jump") and is_on_floor and able_to_jump:
 		apply_central_impulse(Vector3.UP * jump_impulse)
 	
 	if Input.is_action_pressed("player_brake") and is_on_floor:
@@ -90,3 +92,10 @@ func _on_area_3d_area_entered(area):
 		var coins_count = "Coins : " + str(coins)
 		get_parent().get_parent().get_node("GameHUD/Control/LabelCoins").set("text", coins_count)
 		area.free()
+	elif area.is_in_group("jump_zone"):
+		able_to_jump = true
+
+
+func _on_area_3d_area_exited(area):
+	if area.is_in_group("jump_zone"):
+		able_to_jump = false
